3107b501ec18092e2fe60614238c427cfb7229ab
Gabriel Chen <gabriel.jchen@gmail.com>
2021-06-17 13:05:15 -0400

    basic reactbootstap nav

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import "./App.css";
 import dotalogo from "./dotalogo.png";
-import { Nav, Navbar } from "react-bootstrap";
+
 import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark" sticky="bottom">
-        <Navbar.Brand>
-          <img className="logo" src={dotalogo} />
-          Dota-React-Bootstrap
-        </Navbar.Brand>
-        <Nav>
-          <Nav.Link href="heroes">Heroes</Nav.Link>
-          <Nav.Link href="items">Items</Nav.Link>
-          <Nav.Link href="playerstats">Player Stats</Nav.Link>
-        </Nav>
-      </Navbar>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,26 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import "../App.css";
+import dotalogo from "../dotalogo.png";
+import { Nav, Navbar } from "react-bootstrap";
 
-// export default function Navbar() {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-//       <div className="navbar-brand mb-0 h1">Dota-React</div>
-//       <ul>
-//         <li>
-//           <Link to="/heroes">Heroes</Link>
-//         </li>
-//         <li>
-//           <Link to="/items">Items</Link>
-//         </li>
-//         <li>
-//           <Link to="playerstats">Player Stats</Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// }
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function NavbarComponent() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark" sticky="bottom" expand="sm">
+        <Navbar.Brand>
+          <img className="logo" src={dotalogo} />
+          Dota-React-Bootstrap
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="heroes">Heroes</Nav.Link>
+            <Nav.Link href="items">Items</Nav.Link>
+            <Nav.Link href="playerstats">Player Stats</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
