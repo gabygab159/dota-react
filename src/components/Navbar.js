ea2e88b0c7f82117da179315a90261f349051491
Gabriel Chen <gabriel.jchen@gmail.com>
2021-06-17 12:45:43 -0400

    basic react navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// export default function Navbar() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//       <div className="navbar-brand mb-0 h1">Dota-React</div>
+//       <ul>
+//         <li>
+//           <Link to="/heroes">Heroes</Link>
+//         </li>
+//         <li>
+//           <Link to="/items">Items</Link>
+//         </li>
+//         <li>
+//           <Link to="playerstats">Player Stats</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
